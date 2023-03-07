88799258ec31ef34e7bb923c8cb63ee9c99b2957
theunicorntwins <123466747+theunicorntwins@users.noreply.github.com>
2023-03-07 15:49:11 +0700

    Add files via upload

---
+++ EmailApp/EmailApp.java
@@ -0,0 +1,8 @@
+package emailapp;
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("Kayla", "Do");
+
+        System.out.println(em1.showInfo());
+    }
+}
